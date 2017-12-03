c4133c0170ac78a2ba29dd608ea678e1c88627b9
Stefan Fasth <stefan.fasth@gmail.com>
2017-12-03 22:49:22 +0100

    Signup field

---
+++ forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+
+
+class SignupForm(FlaskForm):
+    first_name = StringField("First Name")
+    last_name = StringField("Last Name")
+    email = StringField("Email")
+    password = PasswordField("Password")
+    submit = SubmitField("Sign Up")
+
--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from models import db
-
+from forms import SignupForm
 
 app = Flask(__name__)
 
@@ -10,6 +10,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
 
 db.init_app(app)
 
+app.secret_key = "development-key"
+
 
 @app.route("/")
 def index():
@@ -18,6 +20,15 @@ def index():
 @app.route("/about")
 def about():
   return render_template("about.html")
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignupForm()
+
+    if request.method == "POST":
+        return "Success"
+    elif request.method == "GET":
+        return render_template("signup.html", form=form)
 
 if __name__ == "__main__":
   app.run(debug=True)
