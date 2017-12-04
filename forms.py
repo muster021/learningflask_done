6b0cd9c2fddfa2ecdf8ffd33c754d4bac26d75cd
Stefan Fasth <stefan.fasth@gmail.com>
2017-12-04 19:08:40 +0100

    Signup field

--- forms.py
+++ forms.py
@@ -1,11 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 
 class SignupForm(FlaskForm):
-    first_name = StringField("First Name")
-    last_name = StringField("Last Name")
-    email = StringField("Email")
-    password = PasswordField("Password")
-    submit = SubmitField("Sign Up")
+    first_name = StringField("First Name", validators=[DataRequired("Please enter your first name")])
+    last_name = StringField("Last Name", validators=[DataRequired("Please enter your last name")])
+    email = StringField("Email", validators=[DataRequired("Please enter your email adress"), Email("Please enter a valid email adress")])
+    password = PasswordField("Password", validators=[DataRequired("Please a password"), Length(min=6, message="Password must be 6 characters or more")])
+    submit = SubmitField("Sign Up", validators=[DataRequired()])
 
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired("Please enter your email adress"), Email("Please enter a valid email adress")])
+    password = PasswordField("Password", validators=[DataRequired("Please enter your password")])
+    submit = SubmitField("Sign in")
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug import generate_password_hash, check_password_hash
 
+
 db = SQLAlchemy()
 
 
